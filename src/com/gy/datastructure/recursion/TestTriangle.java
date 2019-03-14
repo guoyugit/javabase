1084dc70e5d23594480b47c2b83173334d6a4cf2
guoyugit <1072489299@qq.com>
2019-03-14 16:35:05 +0800

    递归-数字规律

--- src/com/gy/datastructure/recursion/TestTriangle.java
+++ src/com/gy/datastructure/recursion/TestTriangle.java
@@ -1,11 +1,36 @@
-package com.gy.datastructure.recursion;/**
+package com.gy.datastructure.recursion;
+
+/**
  * ClassName: TestTriangle
  * Description:
  * date: 2019/3/14 11:32
+ * 1/3/6/10/15/21 数字打印规律
+ *
+ * 30,02-25
  *
  * @author 郭宇
  * @since JDK 1.8
  */
 public class TestTriangle {
+    public static void main(String[] args) {
+//        System.out.println(printNumRecursion(1));
+//        System.out.println(printNumRecursion(2));
+//        System.out.println(printNumRecursion(3));
+        System.out.println(printNumRecursion(4));
+    }
 
+    private static int printNum(int i) {
+        if (i < 1) return i;
+        int total = 0;
+        while (i > 0) {
+            total += i;
+            i--;
+        }
+        return total;
+    }
+
+    private static int printNumRecursion(int i) {
+        if (i <= 1) return i;
+        return i + printNumRecursion(--i);
+    }
 }
